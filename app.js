c6310e17819833cfb18b6c4cd313526cfb7c8a6d
Vincent <vincentschwartz7@gmail.com>
2017-07-20 11:22:42 +0200

    heroku setup

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+// app.get('/webhook', function(req, res) {
+//   if (req.query['hub.mode'] === 'subscribe' &&
+//       req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+//     console.log("Validating webhook");
+//     res.status(200).send(req.query['hub.challenge']);
+//   } else {
+//     console.error("Failed validation. Make sure the validation tokens match.");
+//     res.sendStatus(403);          
+//   }  
+// });
+var express = require('express');  
+var bodyParser = require('body-parser');  
+var request = require('request');  
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));  
+app.use(bodyParser.json());  
+app.listen((process.env.PORT || 3000));
+
+// Server frontpage
+app.get('/', function (req, res) {  
+    res.send('This is fb-chatbot Server');
+});
+
+// Facebook Webhook
+app.get('/webhook', function (req, res) {  
+    if (req.query['hub.verify_token'] === 'chatbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
 });
