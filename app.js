cdbd1bb05342ca1502a6837df83991181d2f9c41
Vincent <vincentschwartz7@gmail.com>
2017-07-21 11:16:56 +0200

    echo function

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        // if (event.message && event.message.text) {
-        //     sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        // }else 
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
         if(event.message && event.message.attachments){
         	var lat = event.message.attachments[0].payload.coordinates.lat;
 			var lng = event.message.attachments[0].payload.coordinates.long;
