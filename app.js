3dfe1a09fd034f774efd70b276a10b56cb96c193
Vincent <vincentschwartz7@gmail.com>
2017-07-21 09:46:57 +0200

    begrüßungstext

--- app.js
+++ app.js
@@ -67,3 +67,13 @@ function sendMessage(recipientId, message) {
     });
 
 };
+
+//Begrüßungstext
+curl -X POST -H "Content-Type: application/json" -d '{'
+	"greeting":[
+		{
+			"locale":"default",
+			"text":"Hello {{user_first_name}}!"
+		}
+	]
+'}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=PAGE_ACCESS_TOKEN"
