177b9e1531889fe23d421f5b62209daea5b54c68
Vincent <vincentschwartz7@gmail.com>
2017-07-21 10:13:06 +0200

    no message

--- app.js
+++ app.js
@@ -31,39 +31,105 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-// handler receiving messages
+// recieve messages
 app.post('/webhook', function (req, res) {  
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
+    // var data = req.body.entry[0].messaging;
+    // for (i = 0; i < events.length; i++) {
+    //     var event = events[i];
+    //     if (event.message && event.message.text) {
+    //         sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+    //     }
+    var data = req.body;
+
+    //Check page subscription
+    if(data.object === 'page'){
+    	//Iterate over each entry
+    	data.entry.forEach(function(entry){
+    		var pageID = entry.id;
+    		var timeOfEvent = entry.time;
+
+    		entry.messaging.forEach(function(event) {
+    			if(event.message){
+    				receivedMessage(event);
+    			}else{
+    				console.log("Webhook received unknown event: ", event);
+    			}
+    		});
+    	});
+    	res.sendStatus(200);
+    }
+
    //       if(messageAttachments){
    //      	var lat = event.message.attachments[0].payload.coordinates.lat;
 			// var lng = event.message.attachments[0].payload.coordinates.long;
 			// sendMessage(event.sender.id, {location: "lat/lng: " + lat + "/" + lng});
    //       }
-    }
-    res.sendStatus(200);
 });
 
-// generic function sending messages
-function sendMessage(recipientId, message) {  
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
+function receivedMessage(event){
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
 
-};
+	console.log("Received message for user %d and page %d with message:", senderID, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
+
+	var messageId = message.mid;
+
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+
+	if(messageText){
+		sendTextMsg(senderID);
+	}else if(messageAttachments){
+		sendTextMsg(senderID, "Msg with attachment");
+	}
+}
+
+function sendTextMsg(recipientId, messageText){
+	var messageData = {
+		recipient: {id: recipientId}, message: {text: messageText}
+	};
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData){
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token: PAGE_ACCESS_TOKEN},
+		method: 'POST',
+		json: messageData
+	}, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
+		}else{
+			console.error("Unable to send message.");
+			console.error(response);
+			console.error(error);
+		}
+	});
+}
+
+// generic function sending messages
+// function sendMessage(recipientId, message) {  
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+//         method: 'POST',
+//         json: {
+//             recipient: {id: recipientId},
+//             message: message,
+//         }
+//     }, function(error, response, body) {
+//         if (error) {
+//             console.log('Error sending message: ', error);
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error);
+//         }
+//     });
+
+// };
