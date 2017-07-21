db6cfe53a75f1576c95446df692bcfedb2b10a2b
Vincent <vincentschwartz7@gmail.com>
2017-07-21 10:32:42 +0200

    bug fix

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.post('/webhook', function (req, res) {
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
     }
+    res.sendStatus(200);
 });
 
 //generic function sending messages
